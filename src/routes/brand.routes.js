888ba230941bb36c468c0a3851a4b2ed95666dbe
YouCanCallMeAra <arshgian12@gmail.com>
2021-12-11 15:58:06 +0300

    category,brand  get and post methods added

--- src/routes/brand.routes.js
+++ src/routes/brand.routes.js
@@ -1,9 +1,29 @@
 import { Router } from "express";
+import BrandsModel from "../models/brand.model.js";
+import BrandValidate from "../validations/brand.validate.js";
 
 const brandRouter = Router();
 
-brandRouter.get("/brands", (req, res) => {
-  res.send("hello brands");
+brandRouter.get("/brands", async (req, res) => {
+  const brand = await BrandsModel.find({});
+  res.json(brand);
+});
+
+brandRouter.post("/brands", async (req, res) => {
+  try {
+    await BrandValidate.validateAsync(req.body);
+  } catch (error) {
+    return res.status(400).json(error.message);
+  }
+
+  const brand = new BrandsModel(req.body);
+
+  try {
+    await brand.save();
+  } catch (error) {
+    return res.status(500).json("Unknowen Error Happened!");
+  }
+  res.json({ message: "brand created" });
 });
 
 export default brandRouter;
--- src/routes/category.routes.js
+++ src/routes/category.routes.js
@@ -1,9 +1,29 @@
 import { Router } from "express";
+import CategoriesModel from "../models/category.model.js";
+import CategoryValidate from "../validations/category.validate.js";
 
 const categoryRouter = Router();
 
-categoryRouter.get("/categories", (req, res) => {
-  res.send("hello category");
+categoryRouter.get("/categories", async (req, res) => {
+  const category = await CategoriesModel.find({});
+  res.json(category);
+});
+
+categoryRouter.post("/categories", async (req, res) => {
+  try {
+    await CategoryValidate.validateAsync(req.body);
+  } catch (error) {
+    return res.status(400).json(error.message);
+  }
+
+  const category = new CategoriesModel(req.body);
+
+  try {
+    await category.save();
+  } catch (error) {
+    return res.status(500).json("Unknowen Error Happened!");
+  }
+  res.json({ message: "category created" });
 });
 
 export default categoryRouter;
