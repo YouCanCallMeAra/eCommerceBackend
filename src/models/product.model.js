f5f488b5202d89c602b327302b0fe5062be9cd8e
YouCanCallMeAra <arshgian12@gmail.com>
2021-12-11 15:24:19 +0300

    added product model

---
+++ src/models/product.model.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  brand: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+});
+
+//mongoose.model("User", // name of collection in the db must be capital and singular
+const ProductsModel = mongoose.model("Product", productSchema);
+export default ProductsModel;
